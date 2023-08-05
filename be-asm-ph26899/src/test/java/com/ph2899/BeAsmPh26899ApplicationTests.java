5826bd76ac8256f18b186a5e025d94b6cfcec997
DESKTOP-A8MA4R6\os <toindph26899@fpt.edu.vn>
2023-08-05 23:20:39 +0700

    khoi tao be

---
+++ be-asm-ph26899/src/test/java/com/ph2899/BeAsmPh26899ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ph2899;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAsmPh26899ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
