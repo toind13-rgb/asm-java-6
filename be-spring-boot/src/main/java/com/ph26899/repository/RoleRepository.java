869aa47434509b81a48f3856495eb8d5e2aff35d
DESKTOP-A8MA4R6\os <toindph26899@fpt.edu.vn>
2023-08-06 09:46:02 +0700

    khoi tao role repository

---
+++ be-spring-boot/src/main/java/com/ph26899/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.ph26899.repository;
+
+import com.ph26899.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, String> {
+}
