af55126886433ba3fdd75db60f6710900fccda9e
DESKTOP-A8MA4R6\os <toindph26899@fpt.edu.vn>
2023-08-06 08:51:58 +0700

    khoi tao du an (backend - spring boot)

---
+++ be-spring-boot/src/main/java/com/ph26899/BeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ph26899;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeSpringBootApplication.class, args);
+    }
+
+}
---
+++ be-spring-boot/src/test/java/com/ph26899/BeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ph26899;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
